94a44fb7a8dbb5842ae2f388fdd840d62841a916
NguyenThanhTai <thanhtai5223@gmail.com>
2025-03-14 00:06:21 +0700

    update History Price

--- src/pages/HistoryPrice.tsx
+++ src/pages/HistoryPrice.tsx
@@ -4,7 +4,6 @@ import { HiOutlineChevronRight } from "react-icons/hi";
 import { AiOutlineExport } from "react-icons/ai";
 
 const HistoryPrice = () => {
-
   interface ProductPrice {
     id: number;
     createdAt: string;
@@ -14,20 +13,21 @@ const HistoryPrice = () => {
     sellingPrice: number;
     productPriceStatus: string;
     description: string;
+    productName?: string; // thêm
+    size?: string; // thêm
+    brand?: string; // thêm
   }
 
+
   const [productPrices, setProductPrices] = useState<ProductPrice[]>([]);
-  const [productSizeId, setProductSizeId] = useState(""); // Thêm state cho productSizeId
+  const [productSizeId, setProductSizeId] = useState(""); 
   const [currentPage, setCurrentPage] = useState(1);
+  const [productsAndSizes, setProductsAndSizes] = useState<{name: string, size: string, id: number}[]>([]);
   const rowsPerPage = 5;
   const visiblePageCount = 3;
 
   const totalPages = Math.ceil(productPrices.length / rowsPerPage);
-
-  const startPage = Math.max(
-    Math.min(currentPage - 1, totalPages - visiblePageCount + 1),
-    1
-  );
+  const startPage = Math.max(Math.min(currentPage - 1, totalPages - visiblePageCount + 1), 1);
   const endPage = Math.min(startPage + visiblePageCount - 1, totalPages);
 
   const handlePageChange = (page: number) => {
@@ -36,35 +36,79 @@ const HistoryPrice = () => {
     }
   };
 
-  const paginatedOrders = productPrices.slice(
-    (currentPage - 1) * rowsPerPage,
-    currentPage * rowsPerPage
-  );
+  const paginatedOrders = productPrices.slice((currentPage - 1) * rowsPerPage, currentPage * rowsPerPage);
 
-  
-  const fetchOrders = async () => {
+  // Fetch product prices, product sizes, and product names
+  const fetchPrices = async () => {
     try {
       if (!productSizeId) {
-        alert("Please enter a Product Size ID");
+        alert("Please select a Product Size");
         return;
       }
-      const response = await fetch(
-        `http://localhost:5049/api/ProductPrices/all-product-price/${productSizeId}`
-      );
+
+      const response = await fetch(`http://localhost:5049/api/ProductPrices/all-product-price/${productSizeId}`);
       if (!response.ok) {
-        throw new Error("Failed to fetch data");
+        throw new Error("Failed to fetch Product Prices");
       }
       const data = await response.json();
-      setProductPrices(data); 
+
+      const updatedData = await Promise.all(
+        data.map(async (price: ProductPrice) => {
+          const sizeResponse = await fetch(`http://localhost:5049/api/ProductSizes/${price.productSizeId}`);
+          const sizeData = await sizeResponse.json();
+
+          const productResponse = await fetch(`http://localhost:5049/api/Products/${sizeData.productID}`);
+          const productData = await productResponse.json();
+
+          return {
+            ...price,
+            productName: productData.name,
+            size: sizeData.size,
+            brand: productData.brand,
+          };
+        })
+      );
+
+      setProductPrices(updatedData);
     } catch (error) {
       console.error("Error fetching data: ", error);
     }
   };
 
-  
+
+  const fetchProductNamesAndSizes = async () => {
+    try {
+      const response = await fetch("http://localhost:5049/api/Products"); 
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      const products = await response.json();
+
+      const sizesResponse = await fetch("http://localhost:5049/api/ProductSizes"); 
+      if (!sizesResponse.ok) {
+        throw new Error("Failed to fetch product sizes");
+      }
+      const sizes = await sizesResponse.json();
+
+      const productSizeData = sizes.map((size: any) => ({
+        name: products.find((product: any) => product.productID === size.productID)?.name,
+        size: size.size,
+        id: size.productSizeID,
+      }));
+
+      setProductsAndSizes(productSizeData);
+    } catch (error) {
+      console.error("Error fetching product names and sizes: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProductNamesAndSizes();
+  }, []);
+
   useEffect(() => {
     if (productSizeId) {
-      fetchOrders();
+      fetchPrices();
     }
   }, [productSizeId]);
 
@@ -93,68 +137,76 @@ const HistoryPrice = () => {
               </button>
             </div>
           </div>
-          {/* Input để người dùng nhập Product Size ID */}
+
           <div className="px-4 sm:px-6 lg:px-8 mt-5">
-            <input
-              type="number"
+            {/* Dropdown for Product Name and Size */}
+            <select
               className="w-60 h-10 border dark:bg-blackPrimary bg-white border-gray-600 dark:text-whiteSecondary text-blackPrimary outline-0 indent-10 dark:focus:border-gray-500 focus:border-gray-400"
-              placeholder="Enter Product Size ID"
               value={productSizeId}
-              onChange={(e) => setProductSizeId(e.target.value)} 
-            />
+              onChange={(e) => setProductSizeId(e.target.value)}
+            >
+              <option value="">Select Product Size</option>
+              {productsAndSizes.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.name} - {item.size}
+                </option>
+              ))}
+            </select>
             <button
-              onClick={fetchOrders}
+              onClick={fetchPrices}
               className="ml-2 bg-blue-500 text-white py-2 px-4 rounded"
             >
               Filter
             </button>
           </div>
+
           <table className="min-w-full table-auto border-collapse mt-5">
             <thead>
               <tr>
-                <th className="px-4 py-2 border-b dark:text-whiteSecondary text-blackPrimary">Id</th>
                 <th className="px-4 py-2 border-b dark:text-whiteSecondary text-blackPrimary">Created At</th>
                 <th className="px-4 py-2 border-b dark:text-whiteSecondary text-blackPrimary">Start Date</th>
                 <th className="px-4 py-2 border-b dark:text-whiteSecondary text-blackPrimary">End Date</th>
-                <th className="px-4 py-2 border-b dark:text-whiteSecondary text-blackPrimary">Product Size</th>
+                <th className="px-4 py-2 border-b dark:text-whiteSecondary text-blackPrimary">Product Name</th> 
+                <th className="px-4 py-2 border-b dark:text-whiteSecondary text-blackPrimary">Size</th>
                 <th className="px-4 py-2 border-b dark:text-whiteSecondary text-blackPrimary">Selling Price</th>
                 <th className="px-4 py-2 border-b dark:text-whiteSecondary text-blackPrimary">Status</th>
                 <th className="px-4 py-2 border-b dark:text-whiteSecondary text-blackPrimary">Description</th>
               </tr>
             </thead>
             <tbody>
-  {paginatedOrders.length === 0 ? (
-    <tr>
-      <td colSpan={8} className="px-4 py-6 text-center text-gray-1500">
-        No stock history available for the selected period.
-      </td>
-    </tr>
-  ) : (
-    paginatedOrders.map((HistoryPrice) => (
-      <tr key={HistoryPrice.id}>
-        <td className="px-4 py-6 border-b text-center">{HistoryPrice.id}</td>
-        <td className="px-4 py-5 border-b text-center">{HistoryPrice.createdAt}</td>
-        <td className="px-4 py-6 border-b text-center">{HistoryPrice.startDate}</td>
-        <td className="px-4 py-6 border-b text-center">{HistoryPrice.endDate}</td>
-        <td className="px-4 py-6 border-b text-center">{HistoryPrice.productSizeId}</td>
-        <td className="px-4 py-6 border-b text-center">{HistoryPrice.sellingPrice}</td>
-        <td className="px-4 py-6 border-b text-center">
-        {HistoryPrice.productPriceStatus == "1" ? (
-            <span className="text-green-600">Active</span> 
-        ) : HistoryPrice.productPriceStatus == "2" ? (
-            <span className="text-red-600">Inactive</span> 
-        ) : HistoryPrice.productPriceStatus == "3" ? (
-            <span className="text-yellow-600">PendingForApproval</span> 
-        ) : (
-            <span className="text-gray-600">Unknown</span> 
-        )}
-        </td>
-        <td className="px-4 py-6 border-b text-left">{HistoryPrice.description}</td>
-      </tr>
-    ))
-  )}
-    </tbody>
+              {paginatedOrders.length === 0 ? (
+                <tr>
+                  <td colSpan={9} className="px-4 py-6 text-center text-gray-1500">
+                    No stock history available for the selected period.
+                  </td>
+                </tr>
+              ) : (
+                paginatedOrders.map((HistoryPrice) => (
+                  <tr key={HistoryPrice.id}>
+                    <td className="px-4 py-5 border-b text-center">{HistoryPrice.createdAt}</td>
+                    <td className="px-4 py-6 border-b text-center">{HistoryPrice.startDate}</td>
+                    <td className="px-4 py-6 border-b text-center">{HistoryPrice.endDate}</td>
+                    <td className="px-4 py-6 border-b text-center">{HistoryPrice.productName}</td>
+                    <td className="px-4 py-6 border-b text-center">{HistoryPrice.size}</td>
+                    <td className="px-4 py-6 border-b text-center">{HistoryPrice.sellingPrice}</td>
+                    <td className="px-4 py-6 border-b text-center">
+                      {HistoryPrice.productPriceStatus == "1" ? (
+                        <span className="text-green-600">Active</span>
+                      ) : HistoryPrice.productPriceStatus == "2" ? (
+                        <span className="text-red-600">Inactive</span>
+                      ) : HistoryPrice.productPriceStatus == "3" ? (
+                        <span className="text-yellow-600">PendingForApproval</span>
+                      ) : (
+                        <span className="text-gray-600">Unknown</span>
+                      )}
+                    </td>
+                    <td className="px-4 py-6 border-b text-left">{HistoryPrice.description}</td>
+                  </tr>
+                ))
+              )}
+            </tbody>
           </table>
+
           <div className="flex justify-center items-center px-4 sm:px-6 lg:px-8 py-6">
             <div className="flex gap-2">
               <button
